1ff2687f9568ab10b6001d9ee351a4961271646c
ReactedTitan880 <thomasnardou@icloud.com>
2024-04-19 12:13:14 +0200

    update mainPage

--- FlashCard-Mobile/MauiProgram.cs
+++ FlashCard-Mobile/MauiProgram.cs
@@ -13,6 +13,7 @@ namespace FlashCard_Mobile
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Kanit-Light.ttf", "Kanit");
                 });
 
 #if DEBUG
