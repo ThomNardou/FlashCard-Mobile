26feab8e730378d0dcb749f6187772bd99708119
ReactedTitan880 <thomasnardou@icloud.com>
2024-04-19 11:52:22 +0200

    add project

---
+++ FlashCard-Mobile/FlashCard-Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FlashCard_Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ FlashCard-Mobile/FlashCard-Mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FlashCard_Mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
